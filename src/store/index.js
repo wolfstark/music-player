341b97fe014eba06e582e5e84358243f3f46f3f1
daywatch <daywatch@daywatchdeMacBook-Pro.local>
2017-09-18 09:26:43 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
 import reducers from "../reducers";
 import { routerMiddleware } from "react-router-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-export default function configureStore({initialState,history}) {
+export default function configureStore({ initialState, history }) {
   const middleware = routerMiddleware(history);
 
   return createStore(reducers, composeEnhancers(applyMiddleware(middleware)));
