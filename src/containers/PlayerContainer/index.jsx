63c6b17628d9f25078bae0add3f257557ca02240
wolfstark <>
2017-12-25 19:22:44 +0800

    update

--- src/containers/PlayerContainer/index.jsx
+++ src/containers/PlayerContainer/index.jsx
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { playerHOC } from "../../common/js/HOCs";
+import Player from '../../components/Player'
 
 class PlayerContainer extends Component {
+  static propTypes = {
+    iconMode: PropTypes.func,
+    getFavoriteIcon: PropTypes.fun,
+    toggleFavorite: PropTypes.fun,
+    changeMode: PropTypes.fun,
+    playlist: PropTypes.array.isRequired,
+    sequenceList: PropTypes.array.isRequired,
+    mode: PropTypes.string.isRequired,
+    favoriteList: PropTypes.array.isRequired,
+    currentSong: PropTypes.object.isRequired
+  };
   constructor(props) {
     super(props);
   }
 
   render() {
-    return <div />;
+    return <Player />;
   }
 }
 
